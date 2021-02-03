3f2dd1fd487b706117d6cfefffa833cdb8b70f28
kuku flash <flash.kuku@gmail.com>
2021-02-03 19:37:53 +0200

    Styling work

--- resources/views/pages/cars.blade.php
+++ resources/views/pages/cars.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.kingship')
 @section('content')
 
-<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/subaru.jpg');" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
+    <section class="hero-wrap hero-wrap-3 js-smallheight" style="background-image: url('{{ asset('images/subaru.jpg')}}');" data-stellar-background-ratio="0.5">
+      <div class="overlay"> <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
           <div class="col-md-9 ftco-animate pb-5">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="car_listing">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Cars <i class="ion-ios-arrow-forward"></i></span></p>
+              <p class="breadcrumbs"><span class="mr-2">Home  <i class="ion-ios-arrow-forward"></i> </span>Cars <span><i class="ion-ios-arrow-forward"></i></span></p>
             <h1 class="mb-3 bread">Car listing</h1>
           </div>
         </div>
-      </div>
-     
+      </div></div>
+      
     </section>
 
     <section class="ftco-section contact-section bg-light">
